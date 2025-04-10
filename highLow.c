5af80e2881496c802e6f91495406385bd4c0eda4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-04-10 11:33:52 -0700

    cretaes basic iteration of game loop.

--- highLow.c
+++ highLow.c
@@ -6,7 +6,7 @@ int highLow(char *player1, char *player2, int random);
 
 int main() {
 	srand(time(NULL));
-	int randomNumber = rand() * 100;
+	int randomNumber = rand() % 100;
 
 	char player1Name[20];
 	char player2Name[] = "GAME_MASTER.exe";
@@ -26,6 +26,19 @@ int highLow(char *player1, char *player2, int random) {
 	int guess;
 
 	printf("The random is: %d.\n", random);
-	printf("Select a random number between 0 and 99.");
+	printf("Guess the number between 0 and 99.");
 	scanf_s("%d", &guess);
+
+	if (guess == random) {
+		printf("You win, %s! Way to go!!!", player1);
+		return 0;
+	}
+	if (guess < random) {
+		printf("Sorry, that guess is too low. Try a higher guess.");
+		highLow(player1, player2, random);
+	}
+	else if (guess > random) {
+		printf("Sorry, that guess is too high. Try a lower guess.");
+		highLow(player1, player2, random);
+	}
 }
